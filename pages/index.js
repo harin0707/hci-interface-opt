d48ab33114ff01d8bd0633b728e1e23651790a35
Yu Harin <yuharin0707@naver.com>
2024-11-24 05:14:51 +0900

    [feat] 배포 환경 재 조정

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,11 @@
 export default function Home() {
-    return <h1>Welcome to the Home Page</h1>;
+    return (
+
+        <>
+        <h1>Welcome to the Home Page</h1>;
+
+        <h3>실험자 번호를 입력하세요</h3>
+
+        </>
+    )
 }
