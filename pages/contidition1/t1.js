ae5245b8bfa2b752ab5acb526d2dce23d0b43391
Yu Harin <yuharin0707@naver.com>
2024-11-24 04:48:53 +0900

    [feat] 깃 허브 액션 설정

--- pages/contidition1/t1.js
+++ pages/contidition1/t1.js
@@ -4,5 +4,5 @@ export default function Condition1() {
     const router = useRouter();
     const { id } = router.query; // URL 파라미터 추출
 
-    return <h1>Condition1 Page 기존 방식-  Task Num: {id}</h1>;
+    return <h1>Condition1 Page 기존 방식-  Task Num: 1</h1>;
 }
