fc635374bf71b48a1df1eb8d2760278749188e21
Yu Harin <yuharin0707@naver.com>
2024-11-24 21:23:26 +0900

    Deploy latest build to gh-pages

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    reactStrictMode: true, // 필요한 다른 옵션만 유지
+    reactStrictMode: true, // 기존 설정 유지
+    output: 'export',      // 정적 파일 생성 설정 추가
+    assetPrefix: './',     // GitHub Pages에서 정적 파일 경로를 맞추기 위한 상대 경로 설정
 };
 
 export default nextConfig;
