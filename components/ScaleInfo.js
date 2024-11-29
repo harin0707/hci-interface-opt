4bb300625913a0a3def619090297f797a9126f4e
Yu Harin <yuharin0707@naver.com>
2024-11-30 08:13:24 +0900

    [feat] scale 1.5배 이상이면 글자수 변화

--- components/ScaleInfo.js
+++ components/ScaleInfo.js
@@ -1,6 +1,11 @@
 export const ScaleInfo = ({ scale }) => {
     return (
-        <div style={{ fontWeight: "bold" }}>
+        <div
+            style={{
+                fontWeight: "bold",
+                color: scale >= 1.5 ? "red" : "black", // 조건부 스타일링
+            }}
+        >
             확대율: {scale.toFixed(2)}x
         </div>
     );
